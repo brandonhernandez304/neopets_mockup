7997416470abc3fbd3b2f8a975d0426d47451f3e
Brandon Hernandez <bhernandez30497@gmail.com>
2022-10-21 10:11:26 -0400

    Completed: linked postgresql and initialized main_app

---
+++ Neopets/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Neopets project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Neopets.settings")
+
+application = get_asgi_application()
