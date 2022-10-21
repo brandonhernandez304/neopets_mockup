f86ba966c933c557f9e70c6e2c5aaad057173564
Brandon Hernandez <bhernandez30497@gmail.com>
2022-10-21 12:19:49 -0400

    Compelted: first markdown. All neopets render in index

---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+from django import views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('neopets/', views.neopets_index, name='index'),
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,26 @@
+from unicodedata import name
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse("<h1>Yo yo it's neopets<h1>")
+
+def about(request):
+    return render(request,"about.html")
+
+def neopets_index(request):
+    return render(request, 'neopets/index.html', {'neopets': neopets })
+# Classes
+class Neopet:
+    def __init__(self, name, species, gender, temperament):
+        self.name = name
+        self.species = species
+        self.gender = gender
+        self.temperament = temperament
+
+neopets = [
+    Neopet('Slatty', 'Skeith', 'Male', 'Very Friendly'),
+    Neopet('Litty', 'Grundo', 'Male', 'Dim-witted but good hearted'),
+]
