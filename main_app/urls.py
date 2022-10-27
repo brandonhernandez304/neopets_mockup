276756efd79dca5611ba3a336a562f0a188920e7
Brandon Hernandez <bhernandez30497@gmail.com>
2022-10-27 15:45:10 -0400

    typo in urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -18,5 +18,4 @@ urlpatterns = [
     path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
     path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
     path('accounts/signup/', views.signup, name='signup'),
-    path('cats/<int:neopet_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
 ]
